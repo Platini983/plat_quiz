745d9765f052a900207081ac31af537252326d7b
Michael Amponsah <32392810+Platini983@users.noreply.github.com>
2022-11-14 01:23:25 +0000

    Add files via upload

---
+++ src/components/Ques.jsx
@@ -0,0 +1,72 @@
+export const quest = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "What is the birth year of platini?",
+      answers: [
+        {
+          text: "1994",
+          correct: true,
+        },
+        {
+          text: "1995",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ]
+
+export default {
+  quest,
+}
